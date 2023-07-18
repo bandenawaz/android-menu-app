ff182cc4be72eefedffdab4afb68bb1e5785d2b0
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-07-18 12:10:42 +0530

    Final App

---
+++ app/src/main/java/com/sveri/menuapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.sveri.menuapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        //lets attach menu file
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+       /* switch (item.getItemId()){
+
+            case  R.id.menu_add:
+                Toast.makeText(this, "You selected :"+item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+
+            case  R.id.menu_edit:
+                Toast.makeText(this, "You selected :"+item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+
+            case  R.id.menu_delete:
+                Toast.makeText(this, "You selected :"+item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+
+            case  R.id.menu_settings:
+                Toast.makeText(this, "You selected :"+item.getTitle(), Toast.LENGTH_SHORT).show();
+                break;
+        }*/
+
+        if (item.getItemId() == R.id.menu_add){
+            Toast.makeText(this, "You selected :"+item.getTitle(), Toast.LENGTH_SHORT).show();
+
+        }
+
+        if (item.getItemId() == R.id.menu_edit){
+            Toast.makeText(this, "You selected :"+item.getTitle(), Toast.LENGTH_SHORT).show();
+
+        }
+
+        if (item.getItemId() == R.id.menu_delete){
+            Toast.makeText(this, "You selected :"+item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+
+        if (item.getItemId() == R.id.menu_settings){
+            Toast.makeText(this, "You selected :"+item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
